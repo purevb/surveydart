c1aecfc1a8ef09d6a7cd84300f5cd25932c80bbe
Battsengel <batcengelbatzargal64@gmail.com>
2024-08-15 12:04:20 +0800

    zavaaruulhs umnh save

--- lib/provider/save_provider.dart
+++ lib/provider/save_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:survey/models/answer_options_model.dart';
 import 'package:survey/models/survey_model.dart';
 
 class SaveProvider extends ChangeNotifier {
   List<Survey>? allSurvey = [];
   List<String>? savedMyAnswer = [];
+  List<AnswerOptionModel>? saveResponse = [];
   void addSurvey(List<Survey> surveys) {
     allSurvey?.addAll(surveys);
     notifyListeners();
